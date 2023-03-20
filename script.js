fd0b315d77985c8a3d77e10f7b665f9a0c9deafc
Therese Hagwall <90thejag@skola.boras.se>
2023-03-20 13:15:48 +0100

    changes

---
+++ script.js
@@ -0,0 +1,18 @@
+let pageName = document.getElementById("pageName");
+let description = document.getElementById("description");
+
+async function getPageName() {
+    try {
+        const response = await fetch("https://axlwatch.se/wp-json");
+        const data = await response.json();
+        console.log(data);
+        pageName.innerText = data.name;
+        description.innerText = data.description;
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getPageName();
+
