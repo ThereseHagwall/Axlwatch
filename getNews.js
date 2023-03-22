fd4042be5b6809fc74e2b30b05f9f0d0fe9b35ce
Therese Hagwall <90thejag@skola.boras.se>
2023-03-22 16:52:48 +0100

    Add more changes

--- getNews.js
+++ getNews.js
@@ -4,7 +4,7 @@ export default async function getNews() {
     try {
         const response = await fetch("https://axlwatch.se/wp-json/wp/v2/posts");
         const getData = await response.json();
-
+        console.log(getData);
         printNews(getData)
     } catch (error) {
         console.error(error);
@@ -18,20 +18,22 @@ function printNews(posts) {
     posts.map((post) => {
         const newsContainer = document.createElement("div");
         newsContainer.className = "newsContainer";
+        const info = document.createElement("p");
 
-        const postLi = document.createElement("p");
+        const postLi = document.createElement("h2");
         const aLink = document.createElement("a");
-
+        console.log(post.excerpt.rendered);
         aLink.href = post.link;
         postLi.innerText = post.title.rendered;
+        info.innerText = post.excerpt.rendered;
 
         postLi.addEventListener("click", () => {
             console.log(post.link);
 
         })
-        
+
         postLi.appendChild(aLink);
-        newsContainer.appendChild(postLi)
+        newsContainer.append(postLi, info)
 
         root.append(newsContainer);
     })
