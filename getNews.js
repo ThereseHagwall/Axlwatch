1b2082d90864c9e01beb667397856935394c7aeb
Tony Hagwall <Hagwall86@live.se>
2023-03-22 14:16:28 +0100

    printCategories

--- getNews.js
+++ getNews.js
@@ -1,27 +1,26 @@
 const getWrapper = document.querySelector(".wrapper")
 
-export default async function fetchData() {
+export default async function getNews() {
     try {
         const response = await fetch("https://axlwatch.se/wp-json/wp/v2/posts");
         const getData = await response.json();
 
-        console.log(getData)
         printNews(getData)
     } catch (error) {
         console.error(error)
     }
 }
 
-fetchData()
+getNews()
 
 function printNews(posts) {
     const ul = document.createElement("ul")
-    console.log("funktionen", posts[0].title.rendered)
+
     posts.map((post) => {
 
         const postLi = document.createElement("li")
         const aLink = document.createElement("a")
-        console.log(post.title.rendered)
+
         aLink.href = post.link
         postLi.innerText = post.title.rendered
 
--- getProducts.js
+++ getProducts.js
@@ -3,13 +3,12 @@ let root = document.getElementById("root");
 export default async function getProducts() {
     let response = await fetch("https://axlwatch.se/wp-json/wc/store/products");
     let data = await response.json();
-    console.log("product", data);
 
     data.map(data => {
         let product = document.createElement("div");
         let img = document.createElement("img");
         let price = document.createElement("p");
-        console.log(data.name);
+
         product.innerText = data.name;
         price.innerText = `${data.prices.price} kr`;
         img.src = data.images[0].src;
