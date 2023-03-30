ecd327be69aa9ec1727b67570481c398a87b59e5
Therese Hagwall <90thejag@skola.boras.se>
2023-03-30 14:16:25 +0200

    fix up

--- printProductCart.js
+++ printProductCart.js
@@ -14,7 +14,7 @@ export default function printProductCart() {
 
         fetchProdId(cart)
         async function fetchProdId() {
-            if(cart[i].quantity > 1) {
+            if (cart[i].quantity > 1) {
 
                 var qty = cart[i].quantity;
             } else {
@@ -25,44 +25,44 @@ export default function printProductCart() {
             const prodAmount = document.createElement("p");
             prodAmount.innerText = cart[i].quantity;
 
-        const prodUrl = `https://axlwatch.se/wp-json/wc/v3/products/${cart[i].product_id}`;
-        let response = await fetch(prodUrl);
-        let data = await response.json();
+            const prodUrl = `https://axlwatch.se/wp-json/wc/v3/products/${cart[i].product_id}`;
+            let response = await fetch(prodUrl);
+            let data = await response.json();
 
-        const prodWrapper = document.createElement("div");
-        const prodImg = document.createElement("img");
-        const prodId = document.createElement("p");
-        const prodName = document.createElement("p");
-        const prodPrice = document.createElement("p");
+            const prodWrapper = document.createElement("div");
+            const prodImg = document.createElement("img");
+            const prodId = document.createElement("p");
+            const prodName = document.createElement("p");
+            const prodPrice = document.createElement("p");
 
-        prodImg.className = "cartImg";
-        cartWrapper.className = "cartWrapper";
-        prodWrapper.className = "prodWrapper";
+            prodImg.className = "cartImg";
+            cartWrapper.className = "cartWrapper";
+            prodWrapper.className = "prodWrapper";
 
-        const shippingCost = 100;
+            const shippingCost = 100;
 
-        sumArr.push(parseInt(data.price * qty))
-        prodId.innerText = data.id;
-        prodImg.src = data.images[0].src;
-        prodName.innerText = data.name;
-        prodPrice.innerText = data.price;
+            sumArr.push(parseInt(data.price * qty))
+            prodId.innerText = data.id;
+            prodImg.src = data.images[0].src;
+            prodName.innerText = data.name;
+            prodPrice.innerText = data.price;
 
-        function sumArray(array) {
-            let sum = 0;
+            function sumArray(array) {
+                let sum = 0;
 
-            array.forEach(item => {
-                sum += item;
-            });
+                array.forEach(item => {
+                    sum += item;
+                });
 
-            return sum;
+                return sum;
+            }
+            shippingPrice.innerText = `Frakt: ${shippingCost} kr`;
+            sum.innerText = `Totalsumma: ${sumArray(sumArr) + shippingCost} kr`;
+
+            root.append(cartWrapper)
+            cartWrapper.append(prodWrapper, shippingPrice, sum)
+            prodWrapper.append(prodId, prodImg, prodName, prodPrice, prodAmount)
         }
-        shippingPrice.innerText = `Frakt: ${shippingCost} kr`;
-        sum.innerText = `Totalsumma ${sumArray(sumArr)+ shippingCost}`;
-
-        root.append(cartWrapper)
-        cartWrapper.append(prodWrapper,shippingPrice, sum)
-        prodWrapper.append(prodId, prodImg,prodName,prodPrice,prodAmount)
-    }
-    i++
-});
+        i++
+    });
 }
