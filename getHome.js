06b8ca1dbef8a9f3cf038ccbd15884d995d6443d
Therese Hagwall <90thejag@skola.boras.se>
2023-03-30 15:37:54 +0200

    Landingssidan

--- getHome.js
+++ getHome.js
@@ -9,5 +9,9 @@ export default function getHome() {
 
     const welcome = document.createElement("h1");
     welcome.innerHTML = "Välkommen hit!";
+    const info = document.createElement("h5");
+    info.innerText = "Viverra mauris in aliquam sem fringilla. Eu augue ut lectus arcu bibendum at. Maecenas volutpat blandit aliquam etiam erat velit scelerisque. Natoque penatibus et magnis dis parturient montes nascetur ridiculus. Eu turpis egestas pretium aenean pharetra magna. Ut porttitor leo a diam sollicitudin tempor id. Blandit turpis cursus in hac habitasse platea dictumst quisque sagittis. Non odio euismod lacinia at quis risus sed. Suspendisse potenti nullam ac tortor. Tincidunt ornare massa eget egestas purus viverra.";
+    info.id = "welcomeInfo"
+    welcome.append(info);
     root.append(img, welcome);
 }
